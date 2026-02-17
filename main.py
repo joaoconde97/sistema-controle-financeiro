a3353e1e6c6134fba3203b00608411afa244b36f
joaoconde <jcondedev@gmail.com>
2026-02-16 21:08:14 -0300

    Adicionado as funções de salvar e carregar registros usando JSON

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 def asteriscos():
     print(30 * "*")
 
@@ -37,14 +39,17 @@ def adicionar_registros(registros, tipo):
         "descricao" : descricao
     }
 
+    asteriscos()
     print(f"Tipo: {tipo}")
     print(f"Valor: {valor}")
     print(f"Descrição: {descricao}")
+    asteriscos()
 
     confirmacao = input("Confirmar (s/n)? ")
     
     if confirmacao.lower().strip() == "s":
         registros.append(registro)
+        salvar_registros(registros)
         return registros
     
     elif confirmacao.lower().strip() == "n":
@@ -55,36 +60,53 @@ def adicionar_registros(registros, tipo):
         print("Opção inválida, tente novamente!")
         return registros
 
-registros = []
+def salvar_registros(registros):
+    local_arquivo = "C:\\Users\joaot\Documents\Estudos\Controle-financeiro\\registros.json"
+    with open(local_arquivo,"w")as arquivo:
+        json.dump(registros, arquivo, indent=4)
 
-while True:
+def carregar_registros():
+    try:
+        with open("registros.json", "r")as arquivo:
+            return json.load(arquivo)
 
-    mostrar_menu()
+    except (FileNotFoundError, json.JSONDecodeError):
+        return []
 
-    opcao = input("Digite a opção desejada: ")
-    
-    if opcao.isdigit():
+def main():
+    registros = carregar_registros()
 
-        if opcao == "1":
-            adicionar_registros(registros, "receita")
+    while True:
 
-        elif opcao == "2":
-            adicionar_registros(registros, "despesa")
+        mostrar_menu()
 
-        elif opcao == "3":
-            print("Voce escolheu 3")
-        elif opcao == "4":
-            print("Voce escolheu 4")
-        elif opcao == "5":
-            print("\n Encerrando programa... \n")
-            break
+        opcao = input("Digite a opção desejada: ")
+        
+        if opcao.isdigit():
+
+            if opcao == "1":
+                adicionar_registros(registros, "receita")
+
+            elif opcao == "2":
+                adicionar_registros(registros, "despesa")
+
+            elif opcao == "3":
+                print("Voce escolheu 3")
+            elif opcao == "4":
+                print("Voce escolheu 4")
+            elif opcao == "5":
+                print("\n Encerrando programa... \n")
+                break
+            else:
+                print()
+                print("Opção Inválida, tente novamente!")
+                print()
+
         else:
             print()
             print("Opção Inválida, tente novamente!")
             print()
 
-    else:
-        print()
-        print("Opção Inválida, tente novamente!")
-        print()
 
+if __name__== "__main__":
+    main()
